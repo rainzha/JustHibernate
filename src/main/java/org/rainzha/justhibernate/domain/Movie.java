dc950adf3b484e0e809ed237dd7a03c1ec843d64
rainzha <rainzha@163.com>
2016-05-31 17:34:02 +0800

    搭建hibernate环境.

---
+++ src/main/java/org/rainzha/justhibernate/domain/Movie.java
@@ -0,0 +1,13 @@
+package org.rainzha.justhibernate.domain;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private int id = 0;
+    private String title = null;
+    private String synopsis = null;
+    private String director = null;
+
+
+}
